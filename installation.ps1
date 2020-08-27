d2a27e0172ac23a749beed10424579e4bf30c5c7
akshay4932 <52039278+akshay4932@users.noreply.github.com>
2020-08-27 10:03:18 +0530

    Create installation.ps1

---
+++ installation.ps1
@@ -0,0 +1,7 @@
+$PSVersionTable
+
+#URL for downloading PS 5.1 https://www.microsoft.com/en-us/download/details.aspx?id=54616
+
+Install-Module Az
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Import-Module Az
